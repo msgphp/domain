3a5c19045b8bc7ca79659c1f725c5976b7da377b
Roland Franssen <franssen.roland@gmail.com>
2018-10-28 20:17:18 +0100

    revise doctrine object mappings (#217)

--- Tests/Infra/Doctrine/ObjectMappingsTest.php
+++ Tests/Infra/Doctrine/ObjectMappingsTest.php
@@ -6,10 +6,10 @@ namespace MsgPhp\Domain\Tests\Infra\Doctrine;
 
 use MsgPhp\Domain\Entity\Features;
 use MsgPhp\Domain\Infra\Doctrine\MappingConfig;
-use MsgPhp\Domain\Infra\Doctrine\ObjectFieldMappings;
+use MsgPhp\Domain\Infra\Doctrine\ObjectMappings;
 use PHPUnit\Framework\TestCase;
 
-final class ObjectFieldMappingsTest extends TestCase
+final class ObjectMappingsTest extends TestCase
 {
     public function testMapping(): void
     {
@@ -18,7 +18,7 @@ final class ObjectFieldMappingsTest extends TestCase
         }, glob(\dirname(__DIR__, 3).'/Entity/{Features,Fields}/*.php', \GLOB_BRACE)));
         unset($available[Features\CanBeEnabled::class]);
 
-        $mappings = ObjectFieldMappings::provideObjectFieldMappings(new MappingConfig([]));
+        $mappings = ObjectMappings::provideObjectMappings(new MappingConfig([]));
         $mappings = array_keys($mappings instanceof \Traversable ? iterator_to_array($mappings) : $mappings);
         sort($mappings);
 
